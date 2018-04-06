6f0754e16fad43515e7fe4cde0ab557c37282f02
wilder <champion.bas@gmail.com>
2018-04-06 11:10:14 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 npm run build -- --base-href "https://bastienchampion.github.io/FightWilders/"
-npm run deploy -- --repo="https://github.com/bastienchampion/FightWilders.git"
+npm run deploy -- --repo=https://github.com/bastienchampion/FightWilders.git
