7cc8a664a2d2cd22d22706d1d02753673415da66
AurelieAvisse <aurel.ly28000@gmail.com>
2018-04-06 04:34:29 +0200

    personnage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,52 @@ export class HomeComponent implements OnInit {
   
   results = '';
   heroes
-
+  selectPlayer = null;
+  selectIa = null;
+  healthPlayer = 100;
+  healthIa = 100;
+  
+  getPerso(param) {
+  this.selectPlayer = this.heroes[param];
+  console.log(this.selectPlayer);
+  this.getIa();
+  }
+  getIa() {
+  const choice = Math.floor(Math.random() * 10);
+  switch (choice) {
+  case 0:
+  this.selectIa = this.heroes[560];
+  break;
+  case 1:
+  this.selectIa = this.heroes[550];
+  break;
+  case 2:
+  this.selectIa = this.heroes[540];
+  break;
+  case 3:
+  this.selectIa = this.heroes[530];
+  break;
+  case 4:
+  this.selectIa = this.heroes[520];
+  break;
+  case 5:
+  this.selectIa = this.heroes[510];
+  break;
+  case 6:
+  this.selectIa = this.heroes[500];
+  break;
+  case 7:
+  this.selectIa = this.heroes[490];
+  break;
+  case 8:
+  this.selectIa = this.heroes[480];
+  break;
+  case 9:
+  this.selectIa = this.heroes[470];
+  break;
+  }
+  console.log(this.selectIa);
+  }
   constructor(private http: HttpClient) {
   }
 
